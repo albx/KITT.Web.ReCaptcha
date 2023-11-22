4df95e5f115109779eddd11f4d3c767148e05442
Alberto Mori <albi.mo87@hotmail.it>
2023-11-22 13:52:48 +0100

    #3 - add reCaptcha v3 support for server side validation

---
+++ src/KITT.Web.ReCaptcha.Http/v3/ReCaptchaResponse.cs
@@ -0,0 +1,36 @@
+﻿using System.Text.Json.Serialization;
+
+namespace KITT.Web.ReCaptcha.Http.v3;
+
+public record ReCaptchaResponse
+{
+    /// <summary>
+    /// Gets whether the verification ended successfully
+    /// </summary>
+    [JsonPropertyName("success")]
+    public bool Success { get; init; }
+
+    [JsonPropertyName("score")]
+    public double Score { get; init; }
+
+    [JsonPropertyName("action")]
+    public string Action { get; init; } = string.Empty;
+
+    /// <summary>
+    /// Gets the timestamp of the challenge load
+    /// </summary>
+    [JsonPropertyName("challenge_ts")]
+    public DateTime ChallengeTimestamp { get; init; }
+
+    /// <summary>
+    /// Gets the hostname of the site where the reCAPTCHA was solved
+    /// </summary>
+    [JsonPropertyName("hostname")]
+    public string Hostname { get; init; } = string.Empty;
+
+    /// <summary>
+    /// Gets the optional list of error codes (see https://developers.google.com/recaptcha/docs/verify#error_code_reference)
+    /// </summary>
+    [JsonPropertyName("error-codes")]
+    public IEnumerable<string> ErrorCodes { get; init; } = Enumerable.Empty<string>();
+}
---
+++ src/KITT.Web.ReCaptcha.Http/v3/ReCaptchaService.cs
@@ -0,0 +1,63 @@
+﻿using KITT.Web.ReCaptcha.Http.Configuration;
+using Microsoft.Extensions.Options;
+using System.Net.Http.Json;
+
+namespace KITT.Web.ReCaptcha.Http.v3;
+
+public class ReCaptchaService
+{
+    private readonly HttpClient _httpClient;
+
+    private readonly ReCaptchaConfiguration _configuration;
+
+    public ReCaptchaService(HttpClient httpClient, IOptions<ReCaptchaConfiguration> reCaptchaConfigurationOptions)
+    {
+        _httpClient = httpClient;
+        _configuration = reCaptchaConfigurationOptions?.Value ?? throw new ArgumentNullException(nameof(reCaptchaConfigurationOptions));
+
+        ThrowIfConfigurationIsNotValid(_configuration);
+    }
+
+    public async Task<ReCaptchaResponse> VerifyAsync(string response, string action, string? remoteIp = null, CancellationToken cancellationToken = default)
+    {
+        if (string.IsNullOrWhiteSpace(response))
+        {
+            throw new ArgumentException("value cannot be empty", nameof(response));
+        }
+
+        if (string.IsNullOrWhiteSpace(action))
+        {
+            throw new ArgumentException("value cannot be empty", nameof(action));
+        }
+
+        var content = new FormUrlEncodedContent(new Dictionary<string, string?>
+        {
+            ["secret"] = _configuration.SecretKey,
+            ["response"] = response,
+            ["remoteip"] = remoteIp
+        });
+
+        var reCaptchaResponse = await _httpClient.PostAsync("api/siteverify", content, cancellationToken);
+        reCaptchaResponse.EnsureSuccessStatusCode();
+
+        var responseResult = await reCaptchaResponse.Content.ReadFromJsonAsync<ReCaptchaResponse>(
+            cancellationToken: cancellationToken);
+
+        if (responseResult!.Action != action)
+        {
+            throw new InvalidOperationException("action values does not match");
+        }
+
+        return responseResult!;
+    }
+
+    #region Private methods
+    private static void ThrowIfConfigurationIsNotValid(ReCaptchaConfiguration configuration)
+    {
+        if (string.IsNullOrWhiteSpace(configuration.SecretKey))
+        {
+            throw new ArgumentException("value cannot be empty", nameof(configuration.SecretKey));
+        }
+    }
+    #endregion
+}
