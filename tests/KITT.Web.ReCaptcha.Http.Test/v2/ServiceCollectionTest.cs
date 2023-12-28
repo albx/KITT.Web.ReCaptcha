9557999ec7f5e5308ef198d1572adc9917fe1a3e
Alberto Mori <albi.mo87@hotmail.it>
2023-12-28 22:34:32 +0100

    #17 - add test for service collection extension method

---
+++ tests/KITT.Web.ReCaptcha.Http.Test/v2/ServiceCollectionTest.cs
@@ -0,0 +1,21 @@
+﻿using KITT.Web.ReCaptcha.Http.v2;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace KITT.Web.ReCaptcha.Http.Test.v2;
+
+public class ServiceCollectionTest
+{
+    [Fact]
+    public void AddReCaptchaV2HttpClient_Should_Add_ReCaptchaService_To_Service_Descriptors()
+    {
+        var services = new ServiceCollection();
+
+        ServiceCollectionExtensions.AddReCaptchaV2HttpClient(
+            services,
+            options => options.SecretKey = "secretKey");
+
+        Assert.Contains(
+            services,
+            serviceDescriptor => serviceDescriptor.ServiceType == typeof(ReCaptchaService));
+    }
+}
