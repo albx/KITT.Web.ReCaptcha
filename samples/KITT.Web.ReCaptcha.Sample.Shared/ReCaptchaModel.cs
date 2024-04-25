0d1d7bf9247eead547c8cfbec489204b89b80801
Alberto Mori <albi.mo87@hotmail.it>
2024-04-25 08:28:55 +0200

    #25 - some fixes to sample. remove warnings in tests

--- samples/KITT.Web.ReCaptcha.Sample.Shared/ReCaptchaModel.cs
+++ samples/KITT.Web.ReCaptcha.Sample.Shared/ReCaptchaModel.cs
@@ -1,30 +1,12 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
 
 namespace KITT.Web.ReCaptcha.Sample.Shared;
 
-public class ReCaptchaModel : IValidatableObject
+public class ReCaptchaModel
 {
     [Required]
     public string Text { get; set; } = string.Empty;
 
     public string CaptchaResponse { get; set; } = string.Empty;
-
-    private bool _captchaFromUi;
-
-    public ReCaptchaModel(bool captchaFromUi = false)
-    {
-        _captchaFromUi = captchaFromUi;
-    }
-
-    public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
-    {
-        var result = new List<ValidationResult>();
-
-        if (_captchaFromUi && string.IsNullOrWhiteSpace(CaptchaResponse))
-        {
-            result.Add(new ValidationResult("Captcha is required", [nameof(CaptchaResponse)]));
-        }
-
-        return result;
-    }
 }
